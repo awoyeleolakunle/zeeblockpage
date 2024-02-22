10e8dca5f304e193690909c318a215a663ea06a7
Awoyele Olakunle <awoyeleolakunle@gmail.com>
2024-02-22 18:36:06 +0100

    updated commit

--- src/components/navHeader/zeeLogo/zeeLogo.tsx
+++ src/components/navHeader/zeeLogo/zeeLogo.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import "../zeeLogo/zeelogo.css";
 
 const Zeelogo: React.FC = () => {
-  return <img src="" alt="Not Found" />;
+  return <img src="/assets/Zeeblocks logo 1.png" alt="Not Found" />;
 };
 export default Zeelogo;
